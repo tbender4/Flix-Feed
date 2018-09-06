4da2c004c9642ce97657ff6ffc194e81322253c0
Thomas Bender <tbender4@gmail.com>
2018-09-06 19:38:52 -0400

    finished 2.2

--- Week1Assignment/NowPlayingViewController.swift
+++ Week1Assignment/NowPlayingViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
       //test
         super.viewDidLoad()
+    
+        tableView.dataSource = self
+        
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -25,7 +30,7 @@ class NowPlayingViewController: UIViewController {
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 for movie in movies {
                     let title = movie["title"] as! String
-                    print(title)
+                    //print(title)
                 }
             }
         }
@@ -33,6 +38,16 @@ class NowPlayingViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        return cell
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
