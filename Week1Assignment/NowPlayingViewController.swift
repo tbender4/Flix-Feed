cbf4b9fed86098a3e8461b9c9c2fd56b3bcc75ad
Thomas Bender <tbender4@gmail.com>
2018-09-06 16:13:18 -0400

    Finished 2.1

---
+++ Week1Assignment/NowPlayingViewController.swift
@@ -0,0 +1,53 @@
+//
+//  NowPlayingViewController.swift
+//  Week1Assignment
+//
+//  Created by Thomas Bender on 9/6/18.
+//  Copyright © 2018 Thomas Bender. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+      //test
+        super.viewDidLoad()
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                //print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
