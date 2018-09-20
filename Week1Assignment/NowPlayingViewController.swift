440028e9736b4b24350101428caffe7884645dcb
Thomas Bender <tbender4@gmail.com>
2018-09-20 18:12:00 -0400

    finished first half

--- Week1Assignment/NowPlayingViewController.swift
+++ Week1Assignment/NowPlayingViewController.swift
@@ -34,17 +34,17 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
   }
   
   /*
-  func searchBar (_ searchBar: UISearchBar, textDidChange searchText: String) {
-    filteredMovies = searchText.isEmpty ? movies : movies.filter {
-      let movie = movies[IndexPath.row]
-      let title = movie["title"] as! String
-      
-      (item: title) -> Bool in
-      return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
-    }
-    tableView.reloadData()
-  }
-  */
+   func searchBar (_ searchBar: UISearchBar, textDidChange searchText: String) {
+   filteredMovies = searchText.isEmpty ? movies : movies.filter {
+   let movie = movies[IndexPath.row]
+   let title = movie["title"] as! String
+   
+   (item: title) -> Bool in
+   return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+   }
+   tableView.reloadData()
+   }
+   */
   
   
   @objc func didPullToRefresh (_ refreshControl: UIRefreshControl) {
@@ -117,21 +117,30 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     return cell
   }
   
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let cell = sender as! UITableViewCell
+    if let indexPath = tableView.indexPath(for: cell) {
+      let movie = movies[indexPath.row]
+      let detailViewController = segue.destination as! DetailViewController
+      detailViewController.movie = movie
+    }
   }
-  
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+    
+    
+    override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
