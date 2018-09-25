73ae9ba911bb2f54b81258652579e6f9278ef0b5
Thomas Bender <tbender4@gmail.com>
2018-09-25 18:08:52 -0400

    finished 2.3

---
+++ Week1Assignment/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Week1Assignment
+//
+//  Created by Thomas Bender on 9/20/18.
+//  Copyright © 2018 Thomas Bender. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+  @IBOutlet weak var posterImageView: UIImageView!
+  
+}
